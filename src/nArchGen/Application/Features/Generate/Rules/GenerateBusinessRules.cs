c9631096a4361df3847d04d871809b3012faabef
halaga98 <huso_kildaci@hotmail.com>
2024-12-15 19:43:13 +0300

    Proje dosyası ekle.

---
+++ src/nArchGen/Application/Features/Generate/Rules/GenerateBusinessRules.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using Application.Features.Generate.Constants;
+using Core.CodeGen.File;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.CrossCuttingConcerns.Helpers;
+using Domain.Constants;
+
+namespace Application.Features.Generate.Rules;
+
+public class GenerateBusinessRules
+{
+    public async Task EntityClassShouldBeInhreitEntityBaseClass(string projectPath, string entityName)
+    {
+        string[] fileContent = await File.ReadAllLinesAsync(
+            PlatformHelper.SecuredPathJoin(projectPath, "Pars.RM.RetailApi.MobileStoreApi.Data\\DataModel\\RetailManagementContext", "Table", $"tb_{entityName}.cs")
+        );
+
+        string entityBaseClassNameSpaceUsingTemplate = await File.ReadAllTextAsync(
+            PlatformHelper.SecuredPathJoin(
+                DirectoryHelper.AssemblyDirectory,
+                Templates.Paths.Crud,
+                "Lines",
+                "EntityBaseClassNameSpaceUsing.cs.sbn"
+            )
+        );
+        Regex entityBaseClassRegex = new(@$"public\s+class\s+{entityName}\s*:\s*Entity\s*");
+        bool isExists =
+            fileContent.Any(line =>  fileContent.Any(entityBaseClassRegex.IsMatch));
+
+       
+    }
+
+    public Task FileShouldNotBeExists(string filePath)
+    {
+        if (Directory.Exists(filePath))
+            throw new BusinessException(GenerateBusinessMessages.FileAlreadyExists(filePath));
+        return Task.CompletedTask;
+    }
+}
---
+++ src/nArchGen/ConsoleUI/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using Application;
+using ConsoleUI.Commands.Generate.Command;
+using ConsoleUI.Commands.Generate.Crud;
+using ConsoleUI.Commands.Generate.Query;
+using ConsoleUI.Commands.New;
+using Core.ConsoleUI.IoC.SpectreConsoleCli;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+#region Console Configuration
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
+#endregion
+
+#region IoC
+
+IServiceCollection services = new ServiceCollection();
+services.AddApplicationServices();
+TypeRegistrar registrar = new(services);
+
+#endregion
+
+CommandApp app = new(registrar);
+app.Configure(config =>
+{
+    #region Controller
+
+    config.AddBranch(
+        name: "generate",
+        action: config =>
+        {
+            config.SetDescription("Generate project elements");
+
+            config
+                .AddCommand<GenerateCrudCliCommand>(name: "crud")
+                .WithDescription(description: "Generate CRUD operations for new entity")
+                .WithExample(args: new[] { "generate", "crud", "User", "BaseDbContext" });
+         
+        }
+
+    );
+    config
+    .AddCommand<CreateNewProjectCliCommand>(name: "new")
+    .WithDescription(description: "Create a new project")
+    .WithExample(args: new[] { "new", "ExampleProject" });
+    #endregion
+});
+
+AnsiConsole.Write(new FigletText("halaga").LeftJustified().Color(Color.Blue));
+
+return app.Run(args);
